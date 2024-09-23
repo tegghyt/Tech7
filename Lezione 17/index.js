7764045397382d291826a5b54dcd4b43a1182fe7
Tegghy <166705476+tegghyt@users.noreply.github.com>
2024-09-23 09:00:20 +0200

    c22
    lezione 17

---
+++ Lezione 17/index.js
@@ -0,0 +1,105 @@
+/*let numberOne = 2;
+let numberTwo = 10;
+-+/
+4
+ù
+447if(isNaN(numberOne))§umberTwo){
+    console.log("è uguale");
+}
+else{
+    console.log("non è uguale");
+} */
+/*
+if (numberOne === boolean) {
+    console.log("se è uguale sono falsi")
+}
+
+
+const weather = "sunning";
+
+/*if (weather === "sunning") {
+    console.log("lascio l'ombrello a casa");
+}
+else{
+    console.log("porto l'ombrello");
+}*/
+/*
+if (weather === "sunning") {
+    console.log("porto la crema");
+} else if (weather === "raining") {
+    console.log("porto l'ombrello");
+} else {    
+    console.log("esco normale");
+}
+
+if (numberOne > numberTwo){
+    console.log("num 1 è maggiore di num 2");
+} else {
+    console.log("num 2 è maggiore di num 1");
+}
+
+const questionOne = prompt("quanti anni hai?");
+
+console.log(questionOne);*/
+
+/*const questionOne = Number(prompt("inserisci primo numero?"));
+const questionTwo = Number(prompt("inserisci secondo numero?"));
+
+const questionOperation = prompt("inserisci operazione")
+
+if(questionOperation === "somma"){
+    console.log(questionOne+questionTwo);
+} else if (questionOperation === "sottrazione") {
+    console.log(questionOne-questionTwo);
+} else if (questionOperation === "moltiplicazione") {
+    console.log(questionOne*questionTwo);
+} else if (questionOperation === "divisone") {
+    console.log(questionOne/questionTwo);
+}*/
+
+/*
+const numberOne= Number (prompt("inserisci primo numero"))
+const numberTwo=Number (prompt("inserisci secondo numero"))
+
+const questionOne=prompt("inserisci il tipo operazione")
+if(isNaN(numberOne)){
+    alert("quello che hai scritto non è un numero")
+}
+
+if(questionOne==="somma"){
+    alert(numberOne+numberTwo)
+}else if(questionOne==="sottrazione"){
+    alert(numberOne-numberTwo)
+}else if(questionOne==="moltiplicazione"){
+    alert(numberOne*numberTwo)
+}else if(questionOne==="divisione"){
+    alert(numberOne/numberTwo)}
+
+    else{
+        alert("riscrivi dislessico di merda")
+    } */
+
+/*for (let index = 0; index < 10; index++){
+    console.log(index)
+} */
+/*
+const shoppinglist = ["pane", "acqua","latte"]
+
+for(let index = 0; index < shoppinglist.length; index++){
+    console.log(shoppinglist[index]);
+}*/
+/*
+const shoppinglist = ["pane", "acqua","latte"]
+
+const newItemList = prompt("inserici nuovo elemento")
+
+shoppinglist.push(newItemList)
+
+for(let index = 0; index < shoppinglist.length; index++){
+    console.log(shoppinglist[index]);
+}*/
+
+for(let index = 0; index < 10; index++){
+    const questionOne = prompt("isnerisci nomme prodotto" + index);
+    const questionTwo = prompt("isnerisci nomme prodotto" + index);
+}
