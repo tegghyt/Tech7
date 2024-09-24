98a9e43992a5fc1f02ba4dd9b272b04304428693
Tegghy <166705476+tegghyt@users.noreply.github.com>
2024-09-24 20:42:51 +0200

    c23
    leione 18

---
+++ Lezione 18/index.js
@@ -0,0 +1,27 @@
+
+/*
+const elementH1 = document.getElementById("title")
+const elementH1Class = document.getElementById("title")*/
+/*
+const elementH1 = document.querySelector("#title");
+const elementH1Class = document.querySelector(".title")
+
+console.log(elementH1);
+console.log(elementH1Class);*/
+
+/*
+const elementH1Class = document.querySelector(".title")
+
+console.log(elementH1Class.getBoundingClientRect());*/
+
+const elementButtonMenu = document.querySelector("#button-menu");
+const elementButtonCloseMenu = document.querySelector("#button-close-menu");
+const elementSideBar = document.querySelector("#sidebar");
+
+elementButtonMenu.addEventListener("click", () =>{
+    elementSideBar.classList.add("open")
+})
+
+elementButtonCloseMenu.addEventListener("click", () =>{
+    elementSideBar.classList.remove"open")
+})
